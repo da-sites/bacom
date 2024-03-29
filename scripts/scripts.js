3dbfb229db992ae0a158b21c4527811d6cdd01b7
Chris Millar <cmillar@adobe.com>
2024-03-28 20:35:09 -0600

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -190,15 +190,11 @@ async function loadPage() {
   }
 }
 
-// Side-effects
-(async function daPreview() {
-  const { searchParams } = new URL(window.location.href);
-  const daPreview = searchParams.get('dapreview');
-  if (daPreview) {
-    const origin = daPreview === 'local' ? 'http://localhost:3000' : 'https://da.live';
-    const { default: livePreview } = await import(`${origin}/scripts/dapreview.js`);
-    livePreview(loadPage);
-  }
-}());
-
 loadPage();
+
+(async function livePreview() {
+  const preview = new URL(window.location.href).searchParams.get('dapreview');
+  if (!preview) return;
+  const origin = preview === 'local' ? 'http://localhost:3000' : 'https://da.live';
+  import(`${origin}/scripts/dapreview.js`).then(({ default: daPreview }) => daPreview(loadPage));
+}());
